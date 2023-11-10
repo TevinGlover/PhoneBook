2a3b460c68b497fe67aed05c03e28bcba883cd12
Tevin Glover <tevwork.tg@gmail.com>
2023-11-10 18:14:42 -0500

    working

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,32 +1,41 @@
 package com.zipcodewilmington.phonebook;
 
-import java.util.List;
-import java.util.ArrayList;
+import java.util.*;
 //import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.Map;
+
 
 /**
  * Created by leon on 1/23/18.
  * Made WAY better by kristofer 6/16/20
  */
+
+
+
 public class PhoneBook {
 
     private final Map<String, List<String>> phonebook;
 
     public PhoneBook(Map<String, List<String>> map) {
-        this.phonebook = null;
+
+        this.phonebook = map; //setting to map
     }
 
     public PhoneBook() {
-        this(null);
+        this.phonebook = new HashMap<String, List<String>>();
     }
 
     public void add(String name, String phoneNumber) {
+        ArrayList<String>phoneNumbers = new ArrayList<>();
+        phoneNumbers.add(phoneNumber);
+        this.phonebook.put(name, phoneNumbers);
     }
 
     public void addAll(String name, String... phoneNumbers) {
+        ArrayList<String>phone = new ArrayList<>();
+       for( String number : phoneNumbers){
+           phone.add(number);
+        }
+        this.phonebook.put(name, phone);
     }
 
     public void remove(String name) {
@@ -36,8 +45,13 @@ public class PhoneBook {
         return null;
     }
 
-    public List<String> lookup(String name) {
+    public Boolean hasEntry(String name, String phoneNumber) {
         return null;
+    }
+
+    public List<String> lookup(String name) {
+
+        return  phonebook.get(name);
     }
 
     public String reverseLookup(String phoneNumber)  {
@@ -49,6 +63,6 @@ public class PhoneBook {
     }
 
     public Map<String, List<String>> getMap() {
-        return null;
+        return new HashMap<String, List<String>>(); // seting the getter method to new map
     }
 }
